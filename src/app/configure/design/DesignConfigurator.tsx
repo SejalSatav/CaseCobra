5254f9f5f112a4b3df270db6c58367f1d4a99373
Kshiteej Jain <145803361+SejalSatav@users.noreply.github.com>
2024-07-15 20:07:33 -0400

    adding base price and case material and finish calculations

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -22,7 +22,8 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
-import { Check, ChevronsUpDown } from 'lucide-react'
+import { ArrowRight, Check, ChevronsUpDown } from 'lucide-react'
+import { BASE_PRICE } from '@/config/products'
 
 interface DesignConfiguratorProps {
     configId: string
@@ -254,6 +255,26 @@ const DesignConfigurator = ({
 
                     </div>
                 </ScrollArea>
+
+                <div className='w-full px-8 h-16 bg-white'>
+                    <div className='h-px w-full bg-zinc-200' />
+                    <div className='w-full h-full flex justify-end items-center'>
+                        <div className='w-full flex gap-6 items-center'>
+                            <p className='font-medium whitespace-nowrap'>
+                                {formatPrice(
+                                    (BASE_PRICE + options.finish.price + options.material.price) /
+                                    100
+                                )}
+                            </p>
+                            <Button
+                                size='sm'
+                                className='w-full'>
+                                Continue
+                                <ArrowRight className='h-4 w-4 ml-1.5 inline' />
+                            </Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
